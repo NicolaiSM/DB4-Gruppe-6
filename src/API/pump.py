79dcac5ba87cc2a7b7fa99cca09adbc2679efd36
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-08-07 11:26:52 +0200

    implement forward, backward, speed, off function for pump

--- src/API/pump.py
+++ src/API/pump.py
@@ -1,21 +1,28 @@
 import machine
 
+class Pump(Pump):
 
-class Pump():
+    def __init__(self, pin1, pin2):
+        self.dir = machine.Pin(pin1)
+        self.control = machine.PWM(pin2)
+        self.duty_on = 512
+        self.duty_off = 0
+        self.freqtorotation = 3200
 
-    def __init__(self):
+    def forward(self, value):
+        self.dir.on()
+        self.duty(self.duty_on)
 
-
-
-    def on(self):
-        pass
+    def backward(self, value):
+        self.dir.off()
+        self.duty(self.duty_on)
 
     def off(self):
+        self.duty(self.duty_off)
+
+    def volume(self, value):
         pass
 
-    def volume(self):
-        pass
-
-    def speed(self):
-        pass
+    def speed(self, value):
+        self.freq(value*self.rotationtofreq)
 
