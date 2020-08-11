fd35148229af1e199e3a2d2d9c11eafdd53cb818
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-08-11 11:02:48 +0200

    object for storing data measurement

--- src/data/datastorage.py
+++ src/data/datastorage.py
@@ -1,7 +1,29 @@
-import ucollections
+
+class Queue(list):
+
+    def __init__(self, size):
+        list.__init__([])
+
+        # Max size of list
+        self.size = size
+
+    # Add value to queue
+    def add(self, other):
+        self.append(other)
+
+        if len(self) > self.size:
+            self.pop(0)
 
 
-class Data(ucollections.deque):
+class Datastorage(dict):
 
     def __init__(self):
-        pass
+        dict.__init__({})
+
+    # Add measurement to storage and create a new queue if new measurement
+    def __add__(self, other):
+        key, value = other
+        if not key == self.keys():
+            self[key] = Queue(other[2])
+
+        self[key].append(value)
