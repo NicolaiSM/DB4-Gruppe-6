7a819dab18b0afc863231cc5c3c7070cbcaf3690
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-08-12 14:37:52 +0200

    add average mes to sensor

--- src/API/sensor.py
+++ src/API/sensor.py
@@ -3,8 +3,10 @@ import machine
 
 class Sensor:
 
-    def __init__(self, pin, id, model=None):
+    def __init__(self, pin, nummes, model=None):
         # Setup ADC pin
+        self.mes = []
+        self.nummes = nummes
         self.id = id
         self.adc = machine.ADC(machine.Pin(pin))
         self.adc.atten(machine.ADC.ATTN_11DB)
@@ -18,5 +20,6 @@ class Sensor:
 
     # Get measurement and use model
     def measure(self):
-        return self.model(self.adc.read()), self.id
-
+        for i in range(self.nummes):
+            self.mes.append(self.model(self.adc.read()))
+        return sum(self.mes)/self.nummes
