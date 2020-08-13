1fb97d357979f6d6115d367348e2546d2a6ba820
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-08-13 13:43:10 +0200

    add mqtt thread

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import _thread
 
-from src.Models.tempcontrol import Tempcontrol
+from src.API.cooler import Cooler
+from src.Models.controlloop import Controlloop
+from src.Models.mqtt import Mqtt
+from src.Models.pid import Pid
 
-temppid = Tempcontrol(2 ,_thread.allocate_lock())
+#Cooler
 
+
+
+#Feeder
+
+
+
+
+cooler = Cooler()
+coolpid = Pid(1,1,1,10)
+
+
+mqttapi = {b'Dreambot/feeds/Peltier Element': cooler.setVoltage,
+            b'Dreambot/feeds/Fan': cooler.setFan,
+            b'Dreambot/feeds/P': coolpid.setP,
+            b'Dreambot/feeds/I': coolpid.setI,
+            b'Dreambot/feeds/D': coolpid.setD,
+            }
+
+mqtt = Mqtt(mqttapi)
+
+
+temppid = Controlloop(2, True, lambda x: cooler.speed(coolpid(cooler.measure())))
+
+_thread.start_new_thread(mqtt(), ())
 _thread.start_new_thread(temppid, ())
+
+
+
+
