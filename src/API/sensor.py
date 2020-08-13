98d396f753bc3c93d03aaad4563a9564658231a8
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-08-13 13:38:08 +0200

    add stde to measure function and delete separate stde function

--- src/API/sensor.py
+++ src/API/sensor.py
@@ -21,21 +21,14 @@ class Sensor:
     # Get measurement and use model
     def measure(self):
         self.mes = []
-        for i in range(self.nummes):
-            self.mes.append(self.model(self.adc.read()))
-        return sum(self.mes)/self.nummes
-
-    def stde(self):
-        self.mes = []
-        for i in range(self.nummes):
-            self.mes.append(self.model(self.adc.read()))
-        avg = sum(self.mes)/self.nummes
         error = 0
+
         for i in range(self.nummes):
-            error += (self.mes[i]-avg)**2
-        return (math.sqrt(error/self.nummes))/math.sqrt(self.nummes)
+            self.mes.append(self.model(self.adc.read()))
+        avg = sum(self.mes) / self.nummes
 
+        for i in range(self.nummes):
+            error += (self.mes[i] - avg) ** 2
+        stde = (math.sqrt(error/self.nummes))/math.sqrt(self.nummes)
 
-
-
-
+        return avg,  stde
