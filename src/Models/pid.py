080895ab71e4d43f2d4f5eb2f237d82f902e07ea
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-08-13 09:13:41 +0200

    make pid subclass queue

--- src/Models/pid.py
+++ src/Models/pid.py
@@ -1,17 +1,20 @@
 import time
 
 from src.API.feeder import Feeder
-from src.Models.datastorage import Queue
+from src.Models.queue import Queue
 
 
-class Pid:
+class Pid(Queue):
 
-    def __init__(self, p, i, d, target):
+    def __init__(self, p, i, d, size, target):
+        Queue.__init__(self, size)
         self.p = p
         self.i = i
         self.d = d
+        self.target = target
 
-    def __call__(self):
+    def __call__(self, other):
+        self.add(other-self.target)
         return self.p * self.last() + self.i * self.diff() + self.d * self.sum()
 
     def setP(self, p):
@@ -22,6 +25,9 @@ class Pid:
 
     def setD(self, d):
         self.d = d
+
+
+
 
 
 
