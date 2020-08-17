eadbd073902dca0a7462a71b93a8135473ddb2c7
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-08-17 09:49:18 +0200

    add led to pump

--- src/API/feeder.py
+++ src/API/feeder.py
@@ -1,30 +1,26 @@
 import machine
 
+from src.API.led import Led
 from src.API.pump import Pump
 from src.API.sensor import Sensor
 
 
-class Feeder(Sensor, Pump):
+class Feeder(Sensor, Pump, Led):
 
-    def __init__(self, pin1, pin2, pin3, nummes):
+    def __init__(self, pin1, pin2, pin3, pin4, nummes):
         Sensor.__init__(pin1, nummes)
         Pump.__init__(pin2, pin3)
+        Led.__init__(pin4)
         self.bvd = 0
+
 
     def algaeMeasure(self):
         # k is the number of rotations
-        self.forward(self.bvd)
-        self.rotation(k)
-        self.measure()
-        self.backward(self.bvd)
 
-    def musselMeasure(self):
-        # k is the number of rotations
-        self.backward(self.bvd)
-        self.rotation(k)
         self.measure()
-        self.forward(self.bvd)
+        self.backward()
+        self.rotation()
 
     def feed(self, value):
-        self.forward(self.bvd)
+        self.forward()
         self.rotation(value)
