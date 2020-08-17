62c001a887b69d2f9f356c652a1556b772ee3100
Nicolai Mikkelsen <nicolai.s.m@hotmail.com>
2020-08-17 09:41:37 +0200

    add function to pump that spins the number of rotations

--- src/API/pump.py
+++ src/API/pump.py
@@ -1,4 +1,5 @@
 import machine
+import time
 
 
 class Pump:
@@ -6,6 +7,7 @@ class Pump:
     def __init__(self, pin1, pin2):
         self.pwm = machine.PWM(machine.Pin(pin2))
         self.dir = machine.Pin(pin1, machine.Pin.OUT)
+        self.pin = machine.Pin(pin2, machine.Pin.OUT)
         self.duty_on = 1
         self.duty_off = 0
 
@@ -24,5 +26,13 @@ class Pump:
         pass
 
     def speed(self, value):
-        self.pwm.freq(value)
+        self.pwm.freq(int(value))
 
+    def revs(self, revs):
+        for j in range(revs):
+            print(j)
+            for i in range(3200):
+                print(i)
+                self.pin.on()
+                self.pin.off()
+                time.sleep(1 / 3200)
